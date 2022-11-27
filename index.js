b72d93ca1fced572bea4e85b94582a9201a4dc84
lindaaw <51069424+lindaaw@users.noreply.github.com>
2022-11-27 00:42:12 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
-var randomImage1 = "images/dice" + randomNumber1 + ".png";
+var randomImage1 = "dice" + randomNumber1 + ".png";
 
 var image1 = document.querySelector(".img1");
 
@@ -8,7 +8,7 @@ image1.setAttribute("src", randomImage1);
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-var randomImage2 = "images/dice" + randomNumber2 + ".png";
+var randomImage2 = "dice" + randomNumber2 + ".png";
 
 var image2 = document.querySelector(".img2");
 
